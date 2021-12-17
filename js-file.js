bdda2d4b15e8b74db12eaf1d2cff61b66cc748cb
Joshua <hincheyjosh@gmail.com>
2021-12-17 11:57:16 -0600

    added operate function

--- js-file.js
+++ js-file.js
@@ -12,17 +12,66 @@ const clearAll = () => {
     updateDisplay()
 }
 
-const clear = () => currentInput = null
+const clearCurrent = () => {
+    currentInput = null
+    updateDisplay()
+}
 
 const updateDisplay = () => {
     currentInput === null ? lowerDisplay.textContent = '': lowerDisplay.textContent = currentInput 
-    previousInput === null ? upperDisplay.textContent = '': lowerDisplay.textContent = previousInput
+    previousInput === null ? upperDisplay.textContent = '': upperDisplay.textContent = previousInput
 }
 
-const lowerDisplay = document.querySelector(".lowerDisplay")
-const upperDisplay = document.querySelector(".upperDisplay")
+function operate (number1, number2, operation) {
+    if (operation === 'plus') {
+        return add(number1, number2)
+    } else if (operation === 'minus') {
+        return subtract(number1, number2)
+    } else if (operation === 'multiply') {
+        return multiply(number1, number2)
+    } else if (operation === 'divide') {
+        return divide(number1, number2)
+    } else if (operation === 'modulo') {
+        return modulo(number1, number2)
+    }
+}
 
 let currentInput = null
 let previousInput = null
 let currentOperator = null
 let runningTotal = null
+
+const numbers = document.querySelectorAll(".number")
+const operators = document.querySelectorAll('.operator')
+const lowerDisplay = document.querySelector(".lowerDisplay")
+const upperDisplay = document.querySelector(".upperDisplay")
+const equals = document.querySelector(".equals")
+
+
+
+numbers.forEach(number => {
+    number.addEventListener('click', function() {
+        if (currentInput === null) {
+            currentInput = this.value
+            updateDisplay()
+        } else {
+            currentInput += this.value
+            updateDisplay()
+        }
+    })
+})
+
+operators.forEach(operator => {
+    operator.addEventListener('click', function() {
+        currentOperator = this.value
+        previousInput = currentInput
+        currentInput = null
+        updateDisplay()
+    })
+})
+
+equals.addEventListener('click', function() {
+    runningTotal = operate(Number(previousInput), Number(currentInput), currentOperator)
+    currentInput = runningTotal
+    updateDisplay()
+})
